673e723c644ba4df62c557c019d61b03b499ac79
Ecool88 <kulik0v.e.e@yandex.ru>
2022-06-17 17:23:03 +0300

    (feature): added changes from stage 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,10 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .version('0.0.1')
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
